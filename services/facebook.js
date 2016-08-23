722bb74bb9c054aee7a63e820146aeee0ab5b116
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2016-08-23 13:03:28 +0300

    Cleaned landing.js from facebook logic. All facebook login moved to facebook.js service

--- services/facebook.js
+++ services/facebook.js
@@ -1,5 +1,5 @@
 angular.module('myApp.facebook', [])
-    .factory('facebook', ['$q', '$kinvey', function ($q, $kinvey) {
+    .factory('facebook', ['$q', '$kinvey', '$location', '$rootScope', '$http', 'kinveyConfig', function ($q, $kinvey, $location, $rootScope, $http, kinveyConfig) {
 
         function getLoginStatus() {
             var deferred = $q.defer();
@@ -44,11 +44,154 @@ angular.module('myApp.facebook', [])
             return promise;
         }
 
+        function facebookLogin() {
+            getLoginStatus()
+                .then(function (authResponse) {
+                    var provider = 'facebook';
+                    var tokens = {
+                        'access_token': authResponse.accessToken,
+                        'expires_in': authResponse.expiresIn
+                    };
+                    return Kinvey.User.loginWithProvider(provider, tokens).then(
+                        function (success) {
+                            console.log(success);
+                            console.log("successful login with facebook");
+
+                            $rootScope.currentUser = Kinvey.getActiveUser();
+                            console.log($rootScope.currentUser);
+
+                            return $rootScope.currentUser;
+
+                        }, function (err) {
+                            // Attempt to signup as a new user if no user with the identity exists.
+                            if (Kinvey.Error.USER_NOT_FOUND === err.name) {
+                                console.log("first login with facebook -> registration procedure");
+
+                                return Kinvey.User.signupWithProvider(provider, tokens)
+                                    .then(function (user) {
+                                            return getProfilePicture(user._socialIdentity.facebook.id)
+                                                .then(function (imageData) {
+                                                    console.log(imageData);
+                                                    let profilePicUrl = imageData.data.url;
+                                                    console.log(profilePicUrl);
+
+                                                    return $http.get(profilePicUrl, {responseType: "blob"})
+                                                        .then(function (response) {
+                                                            console.log("https response");
+                                                            console.log(response);
+                                                            let profImageBlob = response.data;
+                                                            return kinveyConfig.authorize
+                                                                .then(function () {
+                                                                    return Kinvey.File.upload(profImageBlob, {
+                                                                        mimeType: profImageBlob.type,
+                                                                        size: profImageBlob.size,
+                                                                        _acl: {
+                                                                            gr: true,
+                                                                            gw: false
+                                                                        }
+                                                                    }, {public: true})
+                                                                        .then(function (success) {
+                                                                            console.log("upload success");
+                                                                            console.log(success);
+                                                                            return success._id;
+                                                                        }, function (error) {
+                                                                            console.log(error);
+                                                                        })
+                                                                        .then(function (imageId) {
+                                                                            // Open comments, likes and dislike for pic
+                                                                            return Kinvey.DataStore.save('pictures', {
+                                                                                image: {
+                                                                                    _type: "KinveyFile",
+                                                                                    _id: imageId
+                                                                                },
+                                                                                _acl: {
+                                                                                    gr: true,
+                                                                                    gw: true
+                                                                                },
+                                                                                comments: [],
+                                                                                votes: {
+                                                                                    likes: [],
+                                                                                    dislikes: []
+                                                                                },
+                                                                                caption: ''
+                                                                            }, {public: true})
+                                                                                .then(function (picture) {
+                                                                                    console.log("picture");
+                                                                                    console.log(picture);
+                                                                                    // Update user and return him
+                                                                                    let rawFacebookName = user._socialIdentity.facebook.name;
+                                                                                    let facebookUsernameParts = rawFacebookName.toLowerCase().split(' ');
+                                                                                    let facebookUsernameReady = facebookUsernameParts.join(".");
+
+                                                                                    user.username = facebookUsernameReady;
+                                                                                    user.profile_picture = picture._id;
+                                                                                    user.fullname = user._socialIdentity.facebook.name;
+                                                                                    user.followersCount = 0;
+                                                                                    user.followingCount = 0;
+
+                                                                                    let social = {
+                                                                                        _id: user._id,
+                                                                                        _acl: {
+                                                                                            gr: true,
+                                                                                            gw: true
+                                                                                        },
+                                                                                        followers: {},
+                                                                                        following: {}
+                                                                                    };
+
+                                                                                    Kinvey.DataStore.save("socials", social)
+                                                                                        .then(function (success) {
+                                                                                            console.log(success);
+                                                                                        }, function (error) {
+                                                                                            console.log(error);
+                                                                                        });
+
+                                                                                    return Kinvey.User.update(user)
+                                                                                        .then(function (updatedUser) {
+                                                                                            $rootScope.currentUser = updatedUser;
+                                                                                            console.log("updatedUser: ");
+                                                                                            console.log(updatedUser);
+                                                                                            console.log("finished successfully");
+                                                                                            return updatedUser;
+                                                                                        }, function (error) {
+                                                                                            console.log(error)
+                                                                                        });
+                                                                                }, function (error) {
+                                                                                    console.log(error);
+                                                                                });
+                                                                        })
+                                                                });
+                                                        })
+                                                }, function (error) {
+                                                    console.log("http response error!:");
+                                                    console.log(error);
+                                                });
+                                        }
+                                    );
+                            }
+                            return Kinvey.Defer.reject(err);
+                        });
+                })
+                .then(function (user) {
+                        if (user) {
+                            console.log("user before path change");
+                            console.log(user);
+                            $rootScope.currentUser = user;
+                            console.log("changing path to profile from landing");
+                            $location.path("/profile");
+                        } else {
+                            console.log("no user passed down so fuck off")
+                        }
+                    },
+                    function (error) {
+                        console.log(error);
+                    });
+        }
 
         return {
-
             getProfilePicture: getProfilePicture,
             updateUserInfo: updateUserInfo,
-            getLoginStatus: getLoginStatus
+            getLoginStatus: getLoginStatus,
+            facebookLogin: facebookLogin
         }
     }]);
